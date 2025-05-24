ceb2de61e3f4e0c792aa4df7c035d17f25cb4125
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2025-05-24 12:55:11 -0400

    Spaces > tabs

--- A3UE/addons/core/Params.hpp
+++ A3UE/addons/core/Params.hpp
@@ -1,14 +1,14 @@
 class Params
 {
     class ExtenderParams;
-	class TitleTestExtender: ExtenderParams
+    class TitleTestExtender: ExtenderParams
     {
         title = "Text Extender Params Section";
         values[] = {""};
         texts[] = {""};
         default = "";
     };
-	class AParamThatDoesSomething: ExtenderParams
+    class AParamThatDoesSomething: ExtenderParams
     {
         title = "Some param that does something or other";
         values[] = {1,2,3,4};
